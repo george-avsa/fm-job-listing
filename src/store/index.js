223d4d14f22cdc347d3fde6197eb6e3a9d18563b
George Avsadzhanishvili <george.avsa@yandex.ru>
2023-02-23 12:34:13 +0300

    'homepage+home'

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { rootReducer } from "./root-reducer";
+import { composeWithDevTools } from "redux-devtools-extension"
 
-const store = createStore(rootReducer);
+const middleware = [];
+
+// const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)));
 
 export {store}
